55ea96c3a6a29f536be4aa5b1688a13e52a6c03c
Kevin Nicolas <kncalderong@gmail.com>
2023-07-18 20:31:03 -0500

    feat: project initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'nunito-sans': ['Nunito Sans', 'sans-serif'],
+      },
+      colors: {
+        'dark-blue': 'hsl(209, 23%, 22%)',
+        'bg-very-dark-blue': 'hsl(207, 26%, 17%)',
+        'text-very-dark-blue': 'hsl(200, 15%, 8%)',
+        'dark-gray': 'hsl(0, 0%, 52%)',
+        'very-light-gray ': 'hsl(0, 0%, 98%)',
+      },
     },
   },
   plugins: [],
