f0a79f51815bc444da03cb4b3608fc58512f13b6
Kevin Nicolas <kncalderong@gmail.com>
2023-07-28 13:39:18 -0500

    refactor: type refactor to allow re-utilize some types on different features

--- src/types/countriesData.ts
+++ src/types/countriesData.ts
@@ -10,17 +10,37 @@ export interface CountryDataType {
     png: string
     svg: string
   }
-  name: {
-    common: string
-    nativeName:
-      | string
-      | {
-          [k: string]: {
-            common: string
-            official?: string
-          }
-        }
-  }
+  name: CountryNameType
   population: number
   region: string
+}
+
+export interface IndividualCountryDataType extends CountryDataType {
+  tld: string[]
+  currencies: CurrenciesObjType
+  subregion: string
+  languages: {
+    [k: string]: string
+  }
+  borders: string[]
+}
+
+export interface CurrenciesObjType {
+  [k: string]: {
+    name: string
+    symbol: string
+  }
+}
+
+export interface CountryNameType {
+  common: string
+  nativeName:
+    | string
+    | {
+        [k: string]: {
+          common: string
+          official: string
+        }
+      }
+  official?: string
 }
